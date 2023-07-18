5679fc6ab414ccac697bb2dd0ec9a0454db132ae
Furse <Fursevich.dasha@mail.ru>
2023-07-18 19:51:55 +0300

    qq

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,45 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        char br1='(';
+        char br2=')';
+        char m='*';
+        char d='/';
+        char sq='^';
+        char s='+';
+        char sub='-';
+        char a='a';
+        char b='b';
+
+        int ang=(a*a-5*b+2+a-7)*((a+b-4*a*b)/2);
+System.out.print("(a*a-5*b+2+a-7)*((a+b-4*a*b)/2)");
+
+        System.out.print(9);
+        System.out.print('a');
+        System.out.print(m);
+        System.out.print('a');
+        System.out.print(sub);
+        System.out.print('5');
+        System.out.print('b');
+        System.out.print(s);
+        System.out.print('2');
+        System.out.print(s);
+        System.out.print('a');
+        System.out.print(sub);
+        System.out.print('7');
+        System.out.print(br2);
+        System.out.print(m);
+        System.out.print(br1);
+        System.out.print('a');
+        System.out.print(s);
+        System.out.print('b');
+        System.out.print(sub);
+        System.out.print('4');
+        System.out.print('a');
+        System.out.print('b');
+        System.out.print(d);
+        System.out.print('2');
+        System.out.print(br2);
+
     }
 }
